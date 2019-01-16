13bb7a1e3e851f122f53b285836bd81a9b5f1aa3
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2019-01-16 09:48:36 +0100

    parallax works

---
+++ assets/js/button.js
@@ -0,0 +1,10 @@
+//JQ Pour la navbar
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+//JQ pour les collapside
+$(document).ready(function () {
+  $('.collapsible').collapsible();
+});
+
---
+++ assets/js/parallax.js
@@ -0,0 +1,4 @@
+//Parallax Initialization 
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
